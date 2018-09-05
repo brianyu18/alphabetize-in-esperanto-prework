4b94815f3c613d5ec09b4b051c46e91f7ec00af8
brianyu18 <brianyu1990@gmail.com>
2018-09-05 22:33:03 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,6 @@
 def alphabetize(arr)
   esperanto_alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
+  p arr
   finish = arr.sort_by do |word|
     taco = word.split("").map do |character|
       esperanto_alphabet.index (character)
